705c93f5111b16a3eb794d0bb95b945de73bd969
Anthony <sean.a.protho1@gmail.com>
2020-07-14 12:59:51 -0700

    fstring error

--- dist/shapes.js
+++ dist/shapes.js
@@ -5,28 +5,34 @@
     class Square extends HTMLElement {
       constructor() {
         super();
-        this._size = "50px";
+        this._size = 50;
         this._color = "red";
         this._shadowRoot = this.attachShadow({ mode: 'open' });
         this._square = document.createElement('div');
         this._square.style.display = 'block';
-        this._square.style.height = this._size;
-        this._square.style.width = this._size;
-        this._square.style.backgroundColor = this._color;
+        this._draw()
         this._shadowRoot.appendChild(this._square);
       }
 
+      _draw() {
+        this._square.style.height = `${this._size}px`;
+        this._square.style.width = `${this._size}px`;
+        this._square.style.backgroundColor = this._color;
+      }
+
       static get observedAttributes() {
-        return ['size']//, 'color'];
+        return ['size', 'color'];
       }
 
       attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'size') {
           this._size = newValue // set the size
           // Resize your shape
+          // ****if unacceptable input use oldValue
         } else if (name === 'color') {
           this._color = newValue // set the color
         }
+        this._draw()
       }
     }
     customElements.define('square-2d', Square);
@@ -35,15 +41,20 @@
   class Rectangle extends HTMLElement {
     constructor() {
       super();
-      this._size = "35px";
+      this._size = 50;
       this._color = "red";
       this._shadowRoot = this.attachShadow({ mode: 'open' });
       this._square = document.createElement('div');
       this._square.style.display = 'block';
-      this._square.style.height = "45px";
-      this._square.style.width = "65px";
-      this._square.style.backgroundColor = this._color;
+      this._draw()
       this._shadowRoot.appendChild(this._square);
+    }
+
+    _draw() {
+      this._square.style.height = `${this._size - 15}px`;
+      this._width = this._size + 15
+      this._square.style.width = `${this._width}px`;
+      this._square.style.backgroundColor = this._color;
     }
 
     static get observedAttributes() {
@@ -53,10 +64,10 @@
     attributeChangedCallback(name, oldValue, newValue) {
       if (name === 'size') {
         this._size = newValue // set the size
-
       } else if (name === 'color') {
         this._color = newValue // set the color
       }
+      this._draw()
     }
   }
   customElements.define('rectangle-2d', Rectangle);
@@ -70,11 +81,15 @@ class Diamond extends HTMLElement {
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._diamond = document.createElement('div');
     this._diamond.style.display = 'block';
+    this._draw()
+    this._diamond.style.transform = "rotate(45deg)";
+    this._shadowRoot.appendChild(this._diamond);
+  }
+
+  _draw() {
     this._diamond.style.height = this._size;
     this._diamond.style.width = this._size;
-    this._diamond.style.transform = "rotate(45deg)";
     this._diamond.style.backgroundColor = this._color;
-    this._shadowRoot.appendChild(this._diamond);
   }
 
   static get observedAttributes() {
@@ -84,10 +99,10 @@ class Diamond extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('diamond-2d', Diamond);
@@ -96,16 +111,20 @@ customElements.define('diamond-2d', Diamond);
 class Parallelogram extends HTMLElement {
   constructor() {
     super();
-    this._size = "50px";
+    this._size = 50;
     this._color = "red";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._parallelogram = document.createElement('div');
     this._parallelogram.style.display = 'block';
-    this._parallelogram.style.height = '45px';
-    this._parallelogram.style.width = this._size;
+    this._draw()
     this._parallelogram.style.transform = "skew(-32deg)";
-    this._parallelogram.style.backgroundColor = this._color;
     this._shadowRoot.appendChild(this._parallelogram);
+  }
+
+  _draw() {
+    this._parallelogram.style.height = `${this._size}px`;
+    this._parallelogram.style.width = `${this._size}px`;
+    this._parallelogram.style.backgroundColor = this._color;
   }
 
   static get observedAttributes() {
@@ -115,10 +134,10 @@ class Parallelogram extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('parallelogram-2d', Parallelogram);
@@ -127,17 +146,21 @@ customElements.define('parallelogram-2d', Parallelogram);
 class Triangle extends HTMLElement {
   constructor() {
     super();
-    this._size = "0";
-    this._color = "none";
+    this._size = 50;
+    this._color = "red";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._triangle = document.createElement('div');
     this._triangle.style.display = 'block';
-    this._triangle.style.height = this._size;
-    this._triangle.style.width = this._size;
-    this._triangle.style.borderLeft = "25px solid transparent";
-    this._triangle.style.borderRight = "25px solid transparent";
-    this._triangle.style.borderBottom = "50px solid red";
+    this._triangle.style.height = "0";
+    this._triangle.style.width = "0";
+    this._draw()
     this._shadowRoot.appendChild(this._triangle);
+  }
+
+  _draw() {
+    this._triangle.style.borderLeft = `${this._size/2}px solid transparent`;
+    this._triangle.style.borderRight = `${this._size/2}px solid transparent`;
+    this._triangle.style.borderBottom = `${this._size}px solid ${this._color}`;
   }
 
   static get observedAttributes() {
@@ -147,10 +170,10 @@ class Triangle extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('triangle-2d', Triangle);
@@ -159,18 +182,22 @@ customElements.define('triangle-2d', Triangle);
 class Scalene extends HTMLElement {
   constructor() {
     super();
-    this._size = "0";
+    this._size = 50;
     this._color = "none";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._scalene = document.createElement('div');
     this._scalene.style.display = 'block';
-    this._scalene.style.height = this._size;
-    this._scalene.style.width = this._size;
-    this._scalene.style.borderLeft = "25px solid transparent";
-    this._scalene.style.borderRight = "25px solid transparent";
-    this._scalene.style.borderBottom = "50px solid red";
-    this._scalene.style.transform = "skew(-32deg)";
+    this._scalene.style.height = "0";
+    this._scalene.style.width = "0";
+    this._draw()
+    this._scalene.style.transform = "skew(-15deg)";
     this._shadowRoot.appendChild(this._scalene);
+  }
+
+  _draw() {
+    this._scalene.style.borderLeft = `${this._size/2}px solid transparent`;
+    this._scalene.style.borderRight = `${this._size/3}px solid transparent`;
+    this._scalene.style.borderBottom = `${this._size}px solid ${this._color}`;
   }
 
   static get observedAttributes() {
@@ -180,10 +207,10 @@ class Scalene extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('scalene-2d', Scalene);
@@ -192,16 +219,20 @@ customElements.define('scalene-2d', Scalene);
 class Right extends HTMLElement {
   constructor() {
     super();
-    this._size = "0";
-    this._color = "none";
+    this._size = 50;
+    this._color = "red";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._right = document.createElement('div');
     this._right.style.display = 'block';
-    this._right.style.height = this._size;
-    this._right.style.width = this._size;
-    this._right.style.borderRight = "50px solid transparent";
-    this._right.style.borderBottom = "50px solid red";
+    this._right.style.height = "0";
+    this._right.style.width = "0";
+    this._draw()
     this._shadowRoot.appendChild(this._right);
+  }
+
+  _draw() {
+    this._right.style.borderRight = `${this._size}px solid transparent`;
+    this._right.style.borderBottom = `${this._size}px solid ${this._color}`;
   }
 
   static get observedAttributes() {
@@ -211,10 +242,10 @@ class Right extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('right-2d', Right);
@@ -233,16 +264,21 @@ customElements.define('right-2d', Right);
 class Circle extends HTMLElement {
   constructor() {
     super();
-    this._size = "50px";
+    this._size = 50;
     this._color = "red";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._circle = document.createElement('div');
     this._circle.style.display = 'block';
-    this._circle.style.height = this._size;
-    this._circle.style.width = this._size;
+    this._draw()
     this._circle.style.borderRadius = "50%";
-    this._circle.style.backgroundColor = this._color;
     this._shadowRoot.appendChild(this._circle);
+  }
+
+  _draw() {
+    this._circle.style.height = `${this._size}px`;
+    this._width = this._size + 15
+    this._circle.style.width = `${this._width}px`;
+    this._circle.style.backgroundColor = this._color;
   }
 
   static get observedAttributes() {
@@ -252,10 +288,10 @@ class Circle extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('circle-2d', Circle);
@@ -264,16 +300,20 @@ customElements.define('circle-2d', Circle);
 class Oval extends HTMLElement {
   constructor() {
     super();
-    this._size = "50px";
+    this._size = 50;
     this._color = "red";
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._oval = document.createElement('div');
     this._oval.style.display = 'block';
-    this._oval.style.height = "45px";
-    this._oval.style.width = "65px";
+    this._draw()
     this._oval.style.borderRadius = "50%";
-    this._oval.style.backgroundColor = this._color;
     this._shadowRoot.appendChild(this._oval);
+  }
+
+  _draw() {
+    this._oval.style.height = `${this._size - 15}px`;
+    this._oval.style.width = `${this._size}px`;
+    this._oval.style.backgroundColor = this._color;
   }
 
   static get observedAttributes() {
@@ -283,10 +323,10 @@ class Oval extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'size') {
       this._size = newValue // set the size
-
     } else if (name === 'color') {
       this._color = newValue // set the color
     }
+    this._draw()
   }
 }
 customElements.define('oval-2d', Oval);
